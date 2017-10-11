c2602e26d184d901fa002ec1dcdd3b11302c9f8a
Larry Peteet <larrypv10@gmail.com>
2017-10-11 15:32:47 -0400

    Modified Server.js for Watson

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
+const appName = require('./package').name;
 
 var app = express();
 
@@ -20,8 +21,17 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/watsonController.js");
 
+//Watson API Related
+const log4js = require('log4js');
+const discovery = require('./public/assets/js/service-manager').get('watson-discovery');
+const logger = log4js.getLogger(appName);
+const serviceManager = require('./public/assets/js/service-manager');
+require('./public/assets/js/index')(app);
+
+//Include our Routes!
 app.use("/", routes);
 
 app.listen(PORT, function() {
+    logger.info(`topic-watson listening on http://localhost:` + PORT);
     console.log("App listening on PORT: " + PORT);
 });
